ba0bca293a6ffb084c79c968d85356b3dfb74928
npt-1707 <npthanh132@gmail.com>
2024-08-05 01:03:30 +0700

    modify model

--- model.py
+++ model.py
@@ -3,11 +3,11 @@ import torch
 
 class CodeBERTModel(torch.nn.Module):
     def __init__(self):
-        self.model = AutoModel.from_pretrained("microsoft/codebert-base")
+        self.encoder = AutoModel.from_pretrained("microsoft/codebert-base")
         self.fc = torch.nn.Linear(768, 2)
         
     def forward(self, input):
-        out = self.model(input)[0]
+        out = self.encoder(input)[0]
         out = torch.mean(out, dim=1)
         out = self.fc(out)
         return out
